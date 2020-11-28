16894561fd76f90359502a1f2e1c69d643dae03f
Adeeth Verma <addyv@Adeeths-MacBook-Pro.local>
2020-11-27 20:35:43 -0500

    test case 4 - product

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -2,8 +2,8 @@
 // DEFINE FUNCTION(S)
 // ==================================================
 function product() {
-  if(num2 == undefined) num2 = 0;
-  if(num1 == undefined) num1 = 0;
+  if(num2 == undefined) num2 = 1;
+  if(num1 == undefined) num1 = 1;
 
   var $result= num1*num2 
   return $result
@@ -25,17 +25,21 @@ try {
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
   var result = product(2, 0);
-  if (result !== 0) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
+  if (result !== 0) throw new Error('Expected product(2, 0) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(2, 2, 2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2, 2, 2 ,2) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = product(2);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
