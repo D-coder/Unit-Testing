58e85ff71287e366b5375e2f0b962b02cceba220
Adeeth Verma <addyv@Adeeths-MacBook-Pro.local>
2020-11-27 20:27:29 -0500

    difference operations

---
+++ 01-unit/01-sum.js
@@ -0,0 +1,50 @@
+// ==================================================
+// DEFINE FUNCTION(S)
+// ==================================================
+function sum(num1, num2) {
+
+  if(num2 == undefined) num2 = 0;
+  if(num1 == undefined) num1 = 0;
+
+  var $result= num1+num2 
+  return $result
+}
+
+// ==================================================
+// DEFINE AND EXECUTE TEST CASES
+// ==================================================
+try {
+  // --------------------------------------------------
+  // Test Case 1
+  // --------------------------------------------------
+  // It should return the sum of two numbers.
+  var result = sum(1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1) to be 2. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 2
+  // --------------------------------------------------
+  // It should ignore additional numbers.
+  var result = sum(1,1,1,1,1);
+  if (result !== 2) throw new Error('Expected sum(1,1,1,1,1) to be 2. Received: ' + result);
+  // --------------------------------------------------
+  // Test Case 3
+  // --------------------------------------------------
+  // When invoked with only 1 number, it should return that number.
+  var result = sum(9);
+  if (result !== 9) throw new Error('Expected sum(9) to be 9. Received: ' + result);
+  // --------------------------------------------------
+  // Test Case 4
+  // --------------------------------------------------
+  // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum(0) to be 0. Received: ' + result);
+  console.log('All tests passed successfully.');
+
+// ==================================================
+// PRINT ERRORS
+// ==================================================
+} catch (e) {
+  console.warn('Whoops, the following test did not pass:');
+  console.error(e.message);
+}
